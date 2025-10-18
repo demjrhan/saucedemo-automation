ca08b0e33c25dc0fad2a48377749cbc6d9669563
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2025-10-18 22:36:09 +0200

    Added more methods to BasePage and made it abstract since object creation is not going to be supported, added Generic return type T to be able to make chain method calls.

--- project/src/main/java/core/BasePage.java
+++ project/src/main/java/core/BasePage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 import java.time.Duration;
 
-public class BasePage {
+public abstract class BasePage {
     protected final WebDriver driver;
     protected final WebDriverWait wait;
     protected final Actions actions;
@@ -19,39 +19,45 @@ public class BasePage {
         this.actions = new Actions(driver);
     }
 
-    protected void clearTextField(By locator) {
+    protected <T> T clearTextField(By locator) {
         WebElement element = findElementVisibility(locator);
         String value = element.getAttribute("value");
-        if (value != null && !value.isEmpty()) {
+        if (value != null && !value.isEmpty())
             element.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
-        }
+        return (T) this;
     }
-    protected void clearTextField(WebElement element) {
+    protected <T> T clearTextField(WebElement element) {
         String value = element.getAttribute("value");
-        if (value != null && !value.isEmpty()) {
+        if (value != null && !value.isEmpty())
             element.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
-        }
+
+        return (T) this;
     }
-    protected void sendKeys(By locator, String text) {
+    protected <T> T sendKeys(By locator, String text) {
         var element = findElementVisibility(locator);
         clearTextField(element);
         element.sendKeys(text);
         wait.until(ExpectedConditions.attributeToBeNotEmpty(element,"value"));
+        return (T) this;
     }
-    protected void sendEnter(By locator){
+    protected <T> T sendEnter(By locator){
         var element = findElementVisibility(locator);
         element.sendKeys(Keys.ENTER);
+        return (T) this;
     }
-    protected void sendTab(By locator){
+    protected <T> T sendTab(By locator){
         var element = findElementVisibility(locator);
         element.sendKeys(Keys.TAB);
+        return (T) this;
     }
-    protected void sendSpace(By locator){
+    protected <T> T sendSpace(By locator){
         var element = findElementVisibility(locator);
         element.sendKeys(Keys.SPACE);
+        return (T) this;
     }
-    protected void click(By locator) {
+    protected <T> T click(By locator) {
         wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+        return (T) this;
     }
     protected WebElement findElementPresence(By locator) {
         return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
@@ -65,28 +71,55 @@ public class BasePage {
     protected List<WebElement> findElementsVisibility(By locator) {
         return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
     }
-    protected void waitUntilElementVisibility(By locator) {
+    protected <T> T waitUntilElementVisibility(By locator) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return (T) this;
     }
-    protected void waitUntilElementInvisibility(By locator) {
+    protected <T> T waitUntilElementInvisibility(By locator) {
         wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+        return (T) this;
     }
-    protected void waitUntilElementPresence(By locator) {
+    protected <T> T waitUntilElementPresence(By locator) {
         wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        return (T) this;
     }
-    protected void scrollToElement(By locator) {
+    protected <T> T scrollToElement(By locator) {
         var element = findElementsPresence(locator);
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        return (T) this;
     }
-    protected void scrollToElement(WebElement element) {
+    protected <T> T scrollToElement(WebElement element) {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        return (T) this;
     }
-    protected void scrollToElementSmooth(By locator) {
+    protected <T> T scrollToElementSmooth(By locator) {
         var element = findElementsPresence(locator);
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({behavior: 'smooth'});", element);
+        return (T) this;
     }
-    protected void scrollToElementSmooth(WebElement element) {
+    protected <T> T scrollToElementSmooth(WebElement element) {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({behavior: 'smooth'});", element);
+        return (T) this;
+    }
+    protected String getText(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).getText().trim();
+    }
+    protected String getTextInside(WebElement parent, By childLocator) {
+        return findInside(parent, childLocator).getText().trim();
+    }
+    protected WebElement findInside(WebElement parent, By childLocator) {
+        return wait.until(_ -> parent.findElement(childLocator));
+    }
+    protected boolean isEmpty(By locator) {
+        WebElement el = findElementVisibility(locator);
+        String value = el.getTagName().equalsIgnoreCase("input") || el.getTagName().equalsIgnoreCase("textarea")
+                ? el.getAttribute("value")
+                : el.getText();
+        return value == null || value.isEmpty();
+    }
+    protected <T> T refresh() {
+        driver.navigate().refresh();
+        return (T) this;
     }
 
 }
