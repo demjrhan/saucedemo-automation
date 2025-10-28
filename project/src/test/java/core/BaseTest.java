7e45595b131b87ef9527ec1153a73e6df0292144
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2025-10-28 19:50:21 +0100

    Made baseTest abstract

--- project/src/test/java/core/BaseTest.java
+++ project/src/test/java/core/BaseTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.DriverManager;
 
-public class BaseTest {
+public abstract class BaseTest {
 
     protected WebDriver driver;
 
